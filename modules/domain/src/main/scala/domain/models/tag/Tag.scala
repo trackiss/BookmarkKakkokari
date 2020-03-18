655eb2927d6721d2439591a10288c88dde5a7567
trackiss <tei123454321@yahoo.co.jp>
2020-03-18 10:59:48 +0900

    Update Tag.scala

--- modules/domain/src/main/scala/domain/models/tag/Tag.scala
+++ modules/domain/src/main/scala/domain/models/tag/Tag.scala
@@ -4,10 +4,12 @@ import java.time.Instant
 
 import domain.models.user.UserId
 import domain.models.ActiveStatus
+import domain.models.bookmark.BookmarkId
 
 final case class Tag(id: TagId,
                      name: TagName,
                      ownerId: UserId,
+                     bookmarkId: BookmarkId,
                      activeStatus: ActiveStatus,
                      createdAt: Instant,
                      updatedAt: Instant)
